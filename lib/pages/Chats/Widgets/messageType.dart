91e35ec7b266218dc0f64f55ff745ee960ec0bf1
SaurabhSain07 <visual.sain100486@gmail>
2024-08-17 12:10:13 +0530

    emoji picker work completely

--- lib/pages/Chats/Widgets/messageType.dart
+++ lib/pages/Chats/Widgets/messageType.dart
@@ -20,86 +20,84 @@ class MessageType extends StatelessWidget {
     RxString message="".obs;
     EmojiPickerController emojiPickerController =Get.put(EmojiPickerController());
     
-    return GestureDetector(
-        onTap: () => FocusScope.of(context).unfocus(),
-      child: Container(
-            margin:const EdgeInsets.all(10),
-            padding:const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.primaryContainer,
-              borderRadius: BorderRadius.circular(100)),
-            child: Row(
-              children: [
-                InkWell(
-                  onTap: (){
-                    emojiPickerController.isEmoji.value =
-                        !emojiPickerController.isEmoji.value;
-                        FocusScope.of(context).unfocus();
+    return Container(
+          margin:const EdgeInsets.all(10),
+          padding:const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            borderRadius: BorderRadius.circular(100)),
+          child: Row(
+            children: [
+              InkWell(
+                onTap: (){
+                  emojiPickerController.isEmoji.value =
+                      !emojiPickerController.isEmoji.value;
+                      FocusScope.of(context).unfocus();
+                      message.value.isNotEmpty;
+                },
+                child: Icon(
+                  Icons.emoji_emotions_outlined,
+                  color: Colors.grey[200],
+                )
+              ),
+              
+              SizedBox(width: 10,),
+              Expanded(
+                child: TextField(
+                  onTap: () {
+                   emojiPickerController.isEmoji.value =false;
                   },
-                  child: Icon(
-                    Icons.emoji_emotions_outlined,
-                    color: Colors.grey[200],
-                  )
+                  onChanged: (value){
+                    message.value=value;
+                    messageController.value;
+                  },
+                  controller: messageController,
+                  decoration:const InputDecoration(
+                    hintText: "Type message...",
+                    filled: false),
+                )
                 ),
-                
-                SizedBox(width: 10,),
-                Expanded(
-                  child: TextField(
-                    onTap: () {
-                      emojiPickerController.isEmoji.value =
-                        !emojiPickerController.isEmoji.value;
-                    },
-                    onChanged: (value){
-                      message.value=value;
-                      messageController.value;
-                    },
-                    controller: messageController,
-                    decoration:const InputDecoration(
-                      hintText: "Type message...",
-                      filled: false),
-                  )
-                  ),
-                const SizedBox(width: 10,),
-                Obx(() => chatController.selectedImagePath.value==""
-                ?InkWell(
-                  onTap: () {
-                    imagePickerBottemSheet(
-                              context,
-                              chatController.selectedImagePath,
-                              imagePickerController);
+              const SizedBox(width: 10,),
+              Obx(() => chatController.selectedImagePath.value==""
+              ?InkWell(
+                onTap: () {
+                  imagePickerBottemSheet(
+                            context,
+                            chatController.selectedImagePath,
+                            imagePickerController);
+                },
+                child: SvgPicture.asset(AssetsImage.gallerySVG))
+                :InkWell(
+                  onTap: (){
+                    chatController.selectedImagePath.value="";
                   },
-                  child: SvgPicture.asset(AssetsImage.gallerySVG))
-                  :InkWell(
-                    onTap: (){
-                      chatController.selectedImagePath.value="";
-                    },
-                    child:const SizedBox(),
-                  ),),
-                const SizedBox(width: 10,),
-                Obx(() => message.value !="" || chatController.selectedImagePath.value!="" || messageController.value !=""
-                ? InkWell(
-                  onTap: () {
-                    if (messageController.text.isNotEmpty ||
-                            chatController.selectedImagePath.value.isNotEmpty) {
-                          chatController.sendMessage(
-                              userModel.id!, messageController.text, userModel);
-                          messageController.clear();
-                          message.value = "";
-                        }
-                  },
-                  child: chatController.isLoading.value 
-                  ?const CircularProgressIndicator()
-                  :SvgPicture.asset(AssetsImage.sendSVG)
-                )  
-                : InkWell(
-                  onTap: (){},
-                  child: SvgPicture.asset(AssetsImage.micSVG))
-                
-               )
-              ],
-            )
-           ),
-    );
+                  child:const SizedBox(),
+                ),),
+              const SizedBox(width: 10,),
+              Obx(() => message.value != "" ||
+                    chatController.selectedImagePath.value != "" ||
+                    messageController.value != ""
+              ? InkWell(
+                onTap: () {
+                  if (messageController.text.isNotEmpty ||
+                          chatController.selectedImagePath.value.isNotEmpty) {
+                        chatController.sendMessage(
+                            userModel.id!, messageController.text, userModel);
+                        messageController.clear();
+                        message.value = "";
+                      }
+                },
+                child: chatController.isLoading.value 
+                ?const CircularProgressIndicator()
+                :SvgPicture.asset(AssetsImage.sendSVG)
+               )  
+              : InkWell(
+                onTap: (){},
+                child: SvgPicture.asset(AssetsImage.micSVG)) 
+             )
+            ],
+          )
+         );
   }
   
 }
--- lib/pages/Chats/chatPage.dart
+++ lib/pages/Chats/chatPage.dart
@@ -31,183 +31,175 @@ class ChatPage extends StatelessWidget {
     // TextEditingController messageController=TextEditingController();
     EmojiPickerController emojiPickerController =Get.put(EmojiPickerController());
     return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: WillPopScope(
-        onWillPop: () {
-          if (emojiPickerController.isEmoji.value) {
-            emojiPickerController.isEmoji.value =
-              !emojiPickerController.isEmoji.value;
-              return Future.value(false);
-          } else {
-            return Future.value(true);
-          }
-        },
-        child: Scaffold(
-          appBar: AppBar(
-            leading: Padding(
-              padding: const EdgeInsets.all(5),
-              child: InkWell(
-                onTap: () {
-                Get.to(UserProfilePage(
-                  userModel: userModel,
-                ));
-              },
-              child: Container(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: CachedNetworkImage(
-                      imageUrl: userModel.profileImage ?? AssetsImage.defaultProfileImage,
-                      fit: BoxFit.fill,
-                      placeholder: (context, url) =>const CircularProgressIndicator(),
-                      errorWidget: (context, url, error) =>const Icon(Icons.error),
-                    ),
-                  ))),
-            ),
-            title: InkWell(
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
+      onTap: () {
+        FocusScope.of(context).unfocus();
+        emojiPickerController.isEmoji.value=false;
+      } ,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Padding(
+            padding: const EdgeInsets.all(5),
+            child: InkWell(
               onTap: () {
-                Get.to(UserProfilePage(
-                  userModel: userModel,
-                ));
-              },
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(userModel.name ?? "User",
-                       style: Theme.of(context).textTheme.bodyLarge,),
-        
-                       StreamBuilder(
-                          stream: chatController.getStatus(userModel.id!),
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState==ConnectionState.waiting) {
-                              return const Text(".....");
-                            }
-                            return Text(
-                              snapshot.data!.status ?? "",
-                              style: TextStyle(
-                                  fontSize: 11,
-                                  color: snapshot.data!.status == "Online"
-                                      ? Colors.green
-                                      : Colors.grey),
-                            );
-                          }) 
-                    ],
+              Get.to(UserProfilePage(
+                userModel: userModel,
+              ));
+            },
+            child: Container(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: CachedNetworkImage(
+                    imageUrl: userModel.profileImage ?? AssetsImage.defaultProfileImage,
+                    fit: BoxFit.fill,
+                    placeholder: (context, url) =>const CircularProgressIndicator(),
+                    errorWidget: (context, url, error) =>const Icon(Icons.error),
                   ),
-                ],
-              ),
-            ),
-            actions: [
-              IconButton(
-                onPressed: () {
-                  callController.callAction(
-                      userModel, profileController.currentUser.value, "audio");
-                      Get.to(AudioCallPage(target: userModel));
-                },
-                icon: const Icon(Icons.call_sharp),
-              ),
-              IconButton(
-                onPressed: () {
-                  callController.callAction(
-                      userModel, profileController.currentUser.value, "video");
-                      Get.to(VideoCallPage(target: userModel));
-                },
-                icon: const Icon(Icons.videocam_outlined),
-              )
-            ],
+                ))),
           ),
-            
-          body: Padding(
-            padding:
-                const EdgeInsets.only(bottom: 10, top: 10, left: 10, right: 10),
-            child: Column(
+          title: InkWell(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            onTap: () {
+              Get.to(UserProfilePage(
+                userModel: userModel,
+              ));
+            },
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Expanded(
-                  child: Stack(
-                    children: [
-                      StreamBuilder(
-                      stream: chatController.getMessage(userModel.id!),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.waiting) {
-                          return const Center(
-                            child: CircularProgressIndicator(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(userModel.name ?? "User",
+                     style: Theme.of(context).textTheme.bodyLarge,),
+      
+                     StreamBuilder(
+                        stream: chatController.getStatus(userModel.id!),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState==ConnectionState.waiting) {
+                            return const Text(".....");
+                          }
+                          return Text(
+                            snapshot.data!.status ?? "",
+                            style: TextStyle(
+                                fontSize: 11,
+                                color: snapshot.data!.status == "Online"
+                                    ? Colors.green
+                                    : Colors.grey),
                           );
-                        }
-                        if (snapshot.hasError) {
-                          return Center(
-                            child: Text("Error: ${snapshot.error}"),
-                          );
-                        }
-                        if (snapshot.data == null) {
-                          return const Center(
-                            child: Text("No Messages"),
-                          );
-                        } else {
-                          return ListView.builder(
-                            reverse: true,
-                            itemCount: snapshot.data!.length,
-                            itemBuilder: (context, index) {
-                              DateTime timestamp = DateTime.parse(
-                                  snapshot.data![index].timestamp!);
-                              String formattedTime =
-                                  DateFormat('hh:mm a').format(timestamp);
-                              return ChatBubble(
-                                message: snapshot.data![index].message!,
-                                imageUrl: snapshot.data![index].imageUrl ?? "",
-                                isComming: snapshot.data![index].receiverId ==
-                                    profileController.currentUser.value.id,
-                                status: "read",
-                                time: formattedTime,
-                              );
-                            },
-                          );
-                        }
-                      },
-                     ),
-                    Obx(() => (chatController.selectedImagePath.value != "")
-                          ? Positioned(
-                              bottom: 0,
-                              left: 0,
-                              right: 0,
-                              child: Stack(
-                                children: [
-                                  Container(
-                                    margin: const EdgeInsets.only(bottom: 10),
-                                    height: 500,
-                                    decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                            image: FileImage(File(chatController
-                                                .selectedImagePath.value)),
-                                            fit: BoxFit.fill),
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .primaryContainer,
-                                        borderRadius: BorderRadius.circular(15)),
-                                  ),
-                          Positioned(
-                            right: 0,
-                                      child: IconButton(
-                                          onPressed: () {
-                                            chatController.selectedImagePath.value="";
-                                          },
-                                          icon:const Icon(Icons.close)))
-                        ],
-                      ),
-                     ):Container())
-                    ],
-                  ),
+                        }) 
+                  ],
                 ),
-                MessageType(userModel: userModel,),
-                Obx(()=>emojiPickerController.isEmoji.value ==true
-                ? EmojiPage()
-                : Container(),
-                )
               ],
             ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                callController.callAction(
+                    userModel, profileController.currentUser.value, "audio");
+                    Get.to(AudioCallPage(target: userModel));
+              },
+              icon: const Icon(Icons.call_sharp),
+            ),
+            IconButton(
+              onPressed: () {
+                callController.callAction(
+                    userModel, profileController.currentUser.value, "video");
+                    Get.to(VideoCallPage(target: userModel));
+              },
+              icon: const Icon(Icons.videocam_outlined),
+            )
+          ],
+        ),
+          
+        body: Padding(
+          padding:
+              const EdgeInsets.only(bottom: 10, top: 10, left: 10, right: 10),
+          child: Column(
+            children: [
+              Expanded(
+                child: Stack(
+                  children: [
+                    StreamBuilder(
+                    stream: chatController.getMessage(userModel.id!),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      if (snapshot.hasError) {
+                        return Center(
+                          child: Text("Error: ${snapshot.error}"),
+                        );
+                      }
+                      if (snapshot.data == null) {
+                        return const Center(
+                          child: Text("No Messages"),
+                        );
+                      } else {
+                        return ListView.builder(
+                          reverse: true,
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            DateTime timestamp = DateTime.parse(
+                                snapshot.data![index].timestamp!);
+                            String formattedTime =
+                                DateFormat('hh:mm a').format(timestamp);
+                            return ChatBubble(
+                              message: snapshot.data![index].message!,
+                              imageUrl: snapshot.data![index].imageUrl ?? "",
+                              isComming: snapshot.data![index].receiverId ==
+                                  profileController.currentUser.value.id,
+                              status: "read",
+                              time: formattedTime,
+                            );
+                          },
+                        );
+                      }
+                    },
+                   ),
+                  Obx(() => (chatController.selectedImagePath.value != "")
+                        ? Positioned(
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            child: Stack(
+                              children: [
+                                Container(
+                                  margin: const EdgeInsets.only(bottom: 10),
+                                  height: 500,
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: FileImage(File(chatController
+                                              .selectedImagePath.value)),
+                                          fit: BoxFit.fill),
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .primaryContainer,
+                                      borderRadius: BorderRadius.circular(15)),
+                                ),
+                        Positioned(
+                          right: 0,
+                                    child: IconButton(
+                                        onPressed: () {
+                                          chatController.selectedImagePath.value="";
+                                        },
+                                        icon:const Icon(Icons.close)))
+                      ],
+                    ),
+                   ):Container())
+                  ],
+                ),
+              ),
+              MessageType(userModel: userModel,),
+              Obx(()=>emojiPickerController.isEmoji.value ==true
+              ? EmojiPage()
+              : Container(),
+              )
+            ],
           ),
         ),
       ),
